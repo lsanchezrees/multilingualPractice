a2edb06d1fe7db11e0d28a19c64d7a8d7acdbb8b
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-15 18:49:05 -0600

    added echo

--- index.php
+++ index.php
@@ -17,10 +17,10 @@ bind_textdomain_codeset("multilingual-practice", "UTF-8");
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php _("A Site About Nothing"); ?></title>
+		<title><?php echo _("A Site About Nothing"); ?></title>
 	</head>
 	<body>
-		<h1><?php _("Ducks"); ?></h1>
-		<p><?php _("I love ducks.  They are so cute."); ?></p>
+		<h1><?php echo _("Ducks"); ?></h1>
+		<p><?php echo _("I love ducks.  They are so cute."); ?></p>
 	</body>
 </html>
