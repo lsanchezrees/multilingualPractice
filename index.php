8ae3b778f717417b02bb8c3a5d67972107c10149
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-15 18:14:37 -0600

    fully functional monolingual site

--- index.php
+++ index.php
@@ -12,3 +12,15 @@ setlocale(LC_ALL, $locale);
 
 bindtextdomain("multilingual-practice", "locale");
 bind_textdomain_codeset("multilingual-practice", "UTF-8");
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php _("A Site About Nothing"); ?></title>
+	</head>
+	<body>
+		<h1><?php _("Ducks"); ?></h1>
+		<p><?php _("I love ducks.  They are so cute."); ?></p>
+	</body>
+</html>
